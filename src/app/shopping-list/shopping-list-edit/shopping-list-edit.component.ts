22f0e79a5b9bfec7fa990b45188aa9c142431dad
Jurgen Gjonçari <jurgen_gjoncari@icloud.com>
2022-07-14 11:22:55 +0200

    Add the Clear and Delete functionality

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,6 +1,7 @@
+import { formatCurrency } from '@angular/common';
 import { Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { from, Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -11,8 +12,7 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingListEditComponent implements OnInit, OnDestroy {
   @Output() ingredientAdded = new EventEmitter<Ingredient>()
-  @ViewChild('form')
-  shoppingListForm!: NgForm;
+  @ViewChild('form') shoppingListForm!: NgForm;
   subscription!: Subscription;
   editMode = false
   editedItemIndex!: number;
@@ -32,12 +32,27 @@ export class ShoppingListEditComponent implements OnInit, OnDestroy {
     })
   }
 
-  onAddItem(form: NgForm) {
-    const newIngredient = new Ingredient(form.value.name, form.value.amount)
-    this.shoppingListService.addIngredients(newIngredient)
-  }
-
   ngOnDestroy(): void {
     this.subscription.unsubscribe()
+  }
+  
+  submitItem(form: NgForm) {
+    const newIngredient = new Ingredient(form.value.name, form.value.amount)
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient)
+    } else {
+      this.shoppingListService.addIngredients(newIngredient)
+    }
+    this.clearForm()
+  }
+
+  clearForm() {
+    this.editMode = false
+    this.shoppingListForm.reset()
+  }
+
+  deleteItem() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex)
+    this.clearForm()
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -2,7 +2,7 @@ import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
-    onIngredientAdded = new Subject<Ingredient[]>()
+    ingredientsChanged = new Subject<Ingredient[]>()
     startedEditing = new Subject<number>()
 
     private ingredients: Ingredient[] = [
@@ -20,5 +20,16 @@ export class ShoppingListService {
 
     addIngredients(...ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients)
+        this.ingredientsChanged.next(this.ingredients.slice())
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient
+        this.ingredientsChanged.next(this.ingredients.slice())
+    }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1)
+        this.ingredientsChanged.next(this.ingredients.slice())
     }
 }

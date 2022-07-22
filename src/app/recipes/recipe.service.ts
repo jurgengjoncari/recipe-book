b5d9dd15926cd9fabc8a8d3bd3ec0c965fb85b7a
Jurgen Gjonçari <j.gjoncari@albansi.com>
2022-07-22 17:32:49 +0200

    Commit

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,7 +11,6 @@ export class RecipeService {
   recipeSelected = new Subject<Recipe>()
   recipesChanged = new Subject<Recipe[]>()
 
-<<<<<<< HEAD
   private recipes!: Recipe[]
 
   // private recipes: Recipe[] = [
@@ -43,36 +42,6 @@ export class RecipeService {
   //     [])
   //   ]
 
-=======
-  private recipes: Recipe[] = [
-    new Recipe('Carbonara',
-      'A classic recipe for carbonara pasta on a table with ingredients',
-      'https://foto.wuestenigel.com/wp-content/uploads/api2/a-classic-recipe-for-carbonara-pasta-on-table-with-ingredients.jpeg',
-    []),
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super tasty Schnitzel - just awesome',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-        [
-          new Ingredient('Meat', 1),
-          new Ingredient('French Fries', 20)
-        ]
-    ),
-    new Recipe(
-      'Big Fat Burger',
-      'What else you need to say?',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ]
-    ),
-    new Recipe('Ceasar salad',
-      'A Caesar salad is a green salad of romaine lettuce and croutons dressed with lemon juice, olive oil, egg, Worcestershire sauce, anchovies, garlic, Dijon mustard, Parmesan cheese, and black pepper. In its original form, this salad was prepared and served tableside. ',
-      'https://c.pxhere.com/photos/db/f5/appetizer_bowl_ceramic_chicken_cooking_crispy_cuisine_delicious-1529253.jpg!d',
-    [])
-    ]
->>>>>>> ea751021e62118a6a48f14b4f09bcd06f483da9e
   getRecipes() {
       return this.recipes
     }
@@ -97,7 +66,6 @@ export class RecipeService {
     this.recipesChanged.next(this.recipes.slice())
   }
 
-<<<<<<< HEAD
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes
     this.recipesChanged.next(this.recipes.slice())

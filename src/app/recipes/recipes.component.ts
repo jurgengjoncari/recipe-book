35e1eeb895fb4e228354c9257923f45917779198
Jurgen Gjonçari <j.gjoncari@albansi.com>
2022-07-25 12:01:05 +0200

    Final touches

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
@@ -11,7 +12,7 @@ import { RecipeService } from './recipe.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe!: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private dataStorageService: DataStorageService) { }
 
   ngOnInit(): void {
     this.recipeService.recipeSelected
@@ -20,5 +21,7 @@ export class RecipesComponent implements OnInit {
           this.selectedRecipe = recipe
       }
     )
+
+    this.dataStorageService.fetchRecipes()
   }
 }
